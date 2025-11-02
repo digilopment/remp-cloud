6a33944a029b781f846dad180e9a82e0d12b893f
Tomáš Doubek <doubek.tomas@markiza.sk>
2025-11-02 21:35:19 +0100

    install cloud

---
+++ installCloud.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+CURRENT_DIR="$PWD"
+
+cd setup && bash clone.sh
+cd $CURRENT_DIR/docker && make docker-build
+docker compose build
+docker compose up -d
+sleep 15
+
+cd $CURRENT_DIR/sites-deployer/Beam && bash install.sh
+cd $CURRENT_DIR/sites-deployer/Campaign && bash install.sh
+cd $CURRENT_DIR/sites-deployer/Crm && bash install.sh
+cd $CURRENT_DIR/sites-deployer/Mailer && bash install.sh
+cd $CURRENT_DIR/sites-deployer/Sso && bash install.sh
+cd $CURRENT_DIR/sites-deployer/Web && bash install.sh
